5f36f4eedd3cb4f08697eb0bafcb53f30ee68d44
Yvan <yfyvan@gmail.com>
2019-06-15 11:39:45 +0800

    a

--- deploy.py
+++ deploy.py
@@ -4,32 +4,32 @@ import wget
 import re
 import shutil
 
-root = '/opt2'
+root_ = '/Users/yvan'
 
 config = {
-    'master': 'sp',
-    'slaves': ['sp1'],
+    'master': 'localhost',
+    'slaves': ['localhost'],
     'dfs.blocksize': 60000000,
-    'dfs.namenode.name.dir': root + '/bigdata/hadoop/hadoop_namenode',
-    'dfs.datanode.data.dir': root + '/bigdata/hadoop/hadoop_datanode',
+    'dfs.namenode.name.dir': root_ + '/bigdata/hadoop/hadoop_namenode',
+    'dfs.datanode.data.dir': root_ + '/bigdata/hadoop/hadoop_datanode',
     'dfs.replication': 1,
     'yarn.resourcemanager.webapp.address.port': 19405,
     'SPARK_MASTER_WEBUI_PORT': 19406
 }
 
 try:
-    os.makedirs(root + '/bigdata/')
+    os.makedirs(root_ + '/bigdata/')
 except:
     pass
 
-root = root + '/bigdata/'
+root = root_ + '/bigdata'
 config.update({'root': root})
-
 
 process = os.popen('echo $JAVA_HOME')  # return file
 JAVA_HOME = process.read()
 process.close()
-# assert JAVA_HOME.find('jdk') >= 0, 'JAVA_HOME配置有问题，请重新配置'
+# JAVA_HOME='/Library/Java/JavaVirtualMachines/jdk1.8.0_211.jdk/Contents/Home'
+assert JAVA_HOME.strip(), 'JAVA_HOME配置有问题，请重新配置'
 print("$JAVA_HOME=" + JAVA_HOME + '\n')
 
 config.update({'JAVA_HOME': JAVA_HOME})
@@ -40,7 +40,8 @@ def soft_download():
     print('安装hadoop。。。\n')
     if not os.path.exists(root + 'hadoop'):
         os.mkdir(root + 'hadoop')
-        wget.download('http://mirrors.tuna.tsinghua.edu.cn/apache/hadoop/common/hadoop-3.1.2/hadoop-3.1.2.tar.gz', root + 'hadoop/hadoop.tar.gz')
+        wget.download('http://mirrors.tuna.tsinghua.edu.cn/apache/hadoop/common/hadoop-3.1.2/hadoop-3.1.2.tar.gz',
+                      root + 'hadoop/hadoop.tar.gz')
         tar = tarfile.open(root + 'hadoop/hadoop.tar.gz', 'r')
         tar.extractall(root + 'hadoop')
         for i in os.listdir(root + 'hadoop'):
@@ -51,7 +52,8 @@ def soft_download():
     print('安装spark。。。\n')
     if not os.path.exists(root + 'spark'):
         os.mkdir(root + 'spark')
-        wget.download('http://mirror.dsrg.utoronto.ca/apache/spark/spark-2.4.3/spark-2.4.3-bin-hadoop2.7.tgz', root + 'spark/spark.tgz')
+        wget.download('http://mirror.dsrg.utoronto.ca/apache/spark/spark-2.4.3/spark-2.4.3-bin-hadoop2.7.tgz',
+                      root + 'spark/spark.tgz')
         tar = tarfile.open(root + 'spark/spark.tgz', 'r')
         tar.extractall(root + 'spark')
         for i in os.listdir(root + 'spark'):
@@ -73,29 +75,29 @@ def soft_download():
     print('安装mysql-connector。。。\n')
     if not os.path.exists(root + 'mysql_connector'):
         os.mkdir(root + 'mysql_connector')
-        wget.download('https://cdn.mysql.com//Downloads/Connector-J/mysql-connector-java_8.0.16-1ubuntu16.04_all.deb', root + 'mysql_connector/mysql_connector.deb')
+        wget.download('https://cdn.mysql.com//Downloads/Connector-J/mysql-connector-java_8.0.16-1ubuntu16.04_all.deb',
+                      root + 'mysql_connector/mysql_connector.deb')
         os.system("""dpkg -X %s %s""" % (root + 'mysql_connector/mysql_connector.deb', root + 'mysql_connector'))
 
 
 # 配置hadoop
 def config_hadoop():
-
     # etc/hadoop/core-site.xml
     with open(root + 'hadoop/hadoop_main/etc/hadoop/core-site.xml', 'r') as f:
         text = f.read()
         f.close()
     text = re.sub('<configuration>[\s\S]+?</configuration>', '''<configuration>
-    
+
                                                                 <property>
                                                                       <name>fs.defaultFS</name>
                                                                       <value>hdfs://%(master)s:9000</value>
                                                                   </property>
-                                                                
+
                                                                   <property>
                                                                       <name>io.file.buffer.size</name>
                                                                       <value>131072</value>
                                                                   </property>
-                                                                  
+
                                                                 </configuration>''' % config, text)
     with open(root + 'hadoop/hadoop_main/etc/hadoop/core-site.xml', 'w') as f:
         f.write(text)
@@ -111,28 +113,28 @@ def config_hadoop():
                                                                       <name>dfs.namenode.name.dir</name>
                                                                       <value>%(dfs.namenode.name.dir)s</value>
                                                                     </property>
-                                                                    
+
                                                                     <property>
                                                                       <name>dfs.blocksize</name>
                                                                       <value>%(dfs.blocksize)s</value>
                                                                     </property>
-                                                                    
+
                                                                     <property>
                                                                       <name>dfs.namenode.handler.count</name>
                                                                       <value>100</value>
                                                                     </property>
-                            
-                                                                    
+
+
                                                                     <property>
                                                                       <name>dfs.datanode.data.dir</name>
                                                                       <value>%(dfs.datanode.data.dir)s</value>
                                                                     </property>
-                                                                    
+
                                                                     <property>
                                                                         <name>dfs.replication</name>
                                                                         <value>%(dfs.replication)s</value>
                                                                     </property>
-                                                                
+
                                                                 </configuration>''' % config, text)
     with open(root + 'hadoop/hadoop_main/etc/hadoop/hdfs-site.xml', 'w') as f:
         f.write(text)
@@ -144,17 +146,17 @@ def config_hadoop():
         f.close()
 
     text = re.sub('<configuration>[\s\S]+?</configuration>', '''<configuration>
-    
+
                                                                     <property>
                                                                       <name>yarn.resourcemanager.hostname</name>
                                                                       <value>%(master)s</value>
                                                                     </property>
-                                                                    
+
                                                                     <property>
                                                                       <name>yarn.resourcemanager.webapp.address</name>
                                                                       <value>0.0.0.0:%(yarn.resourcemanager.webapp.address.port)s</value>
                                                                     </property>
-                                                                    
+
                                                                     <property>
                                                                       <name>yarn.nodemanager.aux-services</name>
                                                                       <value>mapreduce_shuffle</value>
@@ -183,7 +185,7 @@ def config_hadoop():
         f.close()
 
     # etc/hadoop/hadoop-evn.sh
-    with open(root + 'hadoop/hadoop_main/etc/hadoop/hadoop-env.sh.xml', 'a') as f:
+    with open(root + 'hadoop/hadoop_main/etc/hadoop/hadoop-env.sh', 'a') as f:
         f.write('''\n
 export JAVA_HOME=%(JAVA_HOME)s
 export HDFS_NAMENODE_USER="root"
@@ -203,7 +205,6 @@ export HADOOP_SECURE_DN_USER=yarn
 
 
 def config_spark():
-
     # conf/spark-env.sh
     shutil.copy(root + 'spark/spark_main/conf/spark-env.sh.template', root + 'spark/spark_main/conf/spark-env.sh')
     with open(root + 'spark/spark_main/conf/spark-env.sh', 'a') as f:
@@ -215,23 +216,49 @@ export SPARK_MASTER_WEBUI_PORT=%(SPARK_MASTER_WEBUI_PORT)s
         ''' % config)
         f.close()
 
-
     # conf/slaves
     shutil.copy(root + 'spark/spark_main/conf/slaves.template', root + 'spark/spark_main/conf/slaves')
     with open(root + 'spark/spark_main/conf/slaves', 'w') as f:
         f.write('\n'.join(config['slaves']) + '\n' + config['master'])
         f.close()
+    try:
+        # copy mysql-connector into spark jar lib
+        shutil.copy(root + 'mysql_connector/usr/share/java/mysql-connector-java-8.0.16.jar',
+                    root + 'spark/spark_main/jars/mysql-connector-java-8.0.16.jar')
+    except:
+        pass
 
-    # copy mysql-connector into spark jar lib
-    shutil.copy(root + 'mysql_connector/usr/share/java/mysql-connector-java-8.0.16.jar', root + 'spark/spark_main/jars/mysql-connector-java-8.0.16.jar')
 
 def config_bashrc():
-    with open('/root/.bashrc', 'a') as f:
-        f.write('''
+    import sys
+    if sys.platform.find('linux') >= 0:
+        with open('/root/.bashrc', 'a') as f:
+            f.write('''
+    export SPARK_HOME=%(root)s/spark/spark_main
+    export PATH=$PATH:${SPARK_HOME}/bin
+    export PYSPARK_PYTHON=python3
+    
+    export HADOOP_HOME=%(root)s/hadoop/hadoop_main
+    export HADOOP_INSTALL=$HADOOP_HOME
+    export HADOOP_MAPRED_HOME=$HADOOP_HOME
+    export HADOOP_COMMON_HOME=$HADOOP_HOME
+    export HADOOP_HDFS_HOME=$HADOOP_HOME
+    export YARN_HOME=$HADOOP_HOME
+    export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native
+    export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop
+    export PATH=$PATH:$HADOOP_HOME/sbin:$HADOOP_HOME/bin
+    
+    export SCALA_HOME=%(root)s/scala/scala_main
+    export PATH=$PATH:$SCALA_HOME/bin
+            ''' % config)
+            f.close()
+    elif sys.platform.find('darwin') >= 0:
+        with open(root_ + '/.bash_profile', 'a') as f:
+            f.write('''
 export SPARK_HOME=%(root)s/spark/spark_main
 export PATH=$PATH:${SPARK_HOME}/bin
 export PYSPARK_PYTHON=python3
-        
+
 export HADOOP_HOME=%(root)s/hadoop/hadoop_main
 export HADOOP_INSTALL=$HADOOP_HOME
 export HADOOP_MAPRED_HOME=$HADOOP_HOME
@@ -241,12 +268,13 @@ export YARN_HOME=$HADOOP_HOME
 export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native
 export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop
 export PATH=$PATH:$HADOOP_HOME/sbin:$HADOOP_HOME/bin
-        
+
 export SCALA_HOME=%(root)s/scala/scala_main
 export PATH=$PATH:$SCALA_HOME/bin
-        ''' % config)
-        f.close()
+                ''' % config)
+            f.close()
 
-config_hadoop()
-config_spark()
+
+# config_hadoop()
+# config_spark()
 config_bashrc()
